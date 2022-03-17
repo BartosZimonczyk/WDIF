794b52e02d48047e1f06f3df50009592bfc8deed
BartosZimonczyk <bartosz.zimonczyk@gmail.com>
2022-03-17 20:30:22 +0100

    Added few tasks from list 3.

--- List_1/l1z6.R
+++ List_1/l1z6.R
@@ -18,7 +18,7 @@ the_matrix <- matrix(rnorm(500*252, 0, 2),
 the_sums <- apply(the_matrix, 1, cumsum)
 matplot(the_sums, type='l')
 
-the_matrix <- matrix(rnorm(500*252, -1, 2),
+the_matrix <- matrix(rnorm(500*252, -1, 1),
                      nrow = 500,
                      ncol = 252)
 the_sums <- apply(the_matrix, 1, cumsum)
